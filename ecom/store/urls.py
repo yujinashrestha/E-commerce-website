81330fb7db87eab285473e87f0c1c768a7ce67ad
yujinashrestha <yujinashresths@gmail.com>
2025-02-28 13:18:52 +0545

    worked with product page

--- ecom/store/urls.py
+++ ecom/store/urls.py
@@ -7,6 +7,7 @@ urlpatterns=[
   path('login/', views.login_user, name='login'),
   path('logout/', views.logout_user, name='logout'),
   path('register/', views.register_user, name='register'),
+  path('product/<int:pk>', views.product, name='product'),
 
 
 ]
--- ecom/store/views.py
+++ ecom/store/views.py
@@ -51,4 +51,8 @@ def register_user(request):
             return redirect('home')
     else:
         return render(request, 'register.html',  {'form':form})
+    
+def product(request, pk):
+    product=Product.objects.get(id=pk)
+    return render(request, 'product.html', {'product':product})
 
